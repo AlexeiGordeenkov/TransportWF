42f0229a524ff8c4656a9e9a8c95ec8ce3e6395d
VitalSoroka <33748241+VitalSoroka@users.noreply.github.com>
2018-12-22 10:06:55 +0300

    Add Method SaveData

--- Model/Vehicles/Vehicle.cs
+++ Model/Vehicles/Vehicle.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Model
 {
@@ -29,6 +32,42 @@ namespace Model
         abstract public double GetMaxDistance();
         abstract public double GetAcceleratingDistance();
 
+        public void SaveData()
+        {
+            if (log == null)
+                return;
+
+            Excel.Application application = new Excel.Application();
+            Excel.Workbook workbook;
+            Excel.Worksheet worksheet;
+            // Файл шаблона
+            const string template = "template.xlsx";
+
+            // Открываем книгу
+            workbook = application.Workbooks.Open(Path.Combine(Environment.CurrentDirectory, template));
+
+            // Получаем активную таблицу
+            worksheet = workbook.ActiveSheet as Worksheet;
+            int row = 1;
+            foreach (ArrayList arrayList in log)
+            {
+                for(int col = 1; col <= arrayList.Count; col++)
+                {
+                    worksheet.Cells[row, col].Value = arrayList[col - 1];
+                }
+            }
+           // worksheet.Cells[1,1].Value = DateTime.Now;
+            //worksheet.Cells[1,2].Value = this.Name;
+          //  worksheet.Cells[1,3].Value = MaxSpeed;
+          //  worksheet.Cells[1, 4].Value = MaxSpeed;
+
+
+            // Показываем приложение
+            application.Visible = true;
+
+            workbook.SaveAs(Path.Combine(Environment.CurrentDirectory, Name + ".xlsx"));
+
+        }
         protected void Init()
         {
             SetStopPoint();
